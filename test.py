794db804cba2d32204963796c84da5ad71a9b9c9
Ilya Koy <Ilya.koy@outlook.com>
2019-06-28 05:48:28 -0400

    ok something screwy

--- test.py
+++ test.py
@@ -1,32 +1,38 @@
-from network_scanner_module.port_scanner_threading import scan_ports,tcp_connect
-from network_scanner_module.network_scan import network_scan
-from Networks_Hosts import Network
+from network_scanner_module import network_scanning, Networks_Hosts, port_scanner_threading
+
 
 def test_google_open():
-    test_ip1 = tcp_connect('google.com',80)
-    test_ip2 = tcp_connect('google.com',443)
-    test_ip3 = tcp_connect('google.com',53)
-    assert test_ip1 == True
-    assert test_ip2 == True
-    assert test_ip3 == False
+    test_ip1 = tcp_connect("google.com", 80)
+    test_ip2 = tcp_connect("google.com", 443)
+    test_ip3 = tcp_connect("google.com", 53)
+    assert test_ip1 is True
+    assert test_ip2 is True
+    assert test_ip3 is False
+
 
 def test_port_scan_multiple():
-    results = scan_ports('google.com',1,500)
-    assert results[443] == True
-    assert results[80] == True
-    assert results[500] == False
+    results = scan_ports("google.com", 1, 500)
+    assert results[443] is True
+    assert results[80] is True
+    assert results[500] is False
+
 
 def test_ping_scan_google_net():
-    ping = network_scan("172.217.7.0","24")
-    #172.217.7.14 is google.com
-    assert ping['172.217.7.14'] == True
+    ping = network_scan("172.217.7.0", "24")
+    # 172.217.7.14 is google.com
+    assert ping["172.217.7.14"] is True
 
-    ping_name = network_scan('google.com',hostname=True)
-    ping_name1 = network_scan('IKhammer.com',hostname=True)
-    assert ping_name['google.com'] == True
-    assert ping_name1['IKhammer.com'] == False
+    ping_name = network_scanning.network_scan("google.com", hostname=True)
+    ping_name1 = network_scanning.network_scan("IKhammer.com", hostname=True)
+    assert ping_name["google.com"] is True
+    assert ping_name1["IKhammer.com"] is False
+
 
 def test_hosts():
-    cloud_flare = Network('173.245.48.0',20)
+    cloud_flare = Network("173.245.48.0", 20)
     cloud_flare.discover_hosts()
     cloud_flare.discover_hostnames()
+
+
+if __name__ == "__main__":
+    pass
